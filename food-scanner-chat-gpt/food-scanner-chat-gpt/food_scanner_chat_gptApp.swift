c8b20d2e3cdda42635482d511cccd3c34b037803
kris-randen <kris.randen@gmail.com>
2023-02-25 18:11:14 +0530

    Init.

---
+++ food-scanner-chat-gpt/food-scanner-chat-gpt/food_scanner_chat_gptApp.swift
@@ -0,0 +1,17 @@
+//
+//  food_scanner_chat_gptApp.swift
+//  food-scanner-chat-gpt
+//
+//  Created by Krishnaswami Rajendren on 2/25/23.
+//
+
+import SwiftUI
+
+@main
+struct food_scanner_chat_gptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
